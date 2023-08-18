c215b4489a767c422c4523f25edfe3d40249d346
SandarvaShakya <sandarvashakya34@gmail.com>
2023-08-18 13:24:41 +0545

    🛠 Fix transition of nav bar and fix some width of the hero section in laptop view

--- js/index.js
+++ js/index.js
@@ -13,10 +13,13 @@ window.addEventListener('scroll', () => {
     let pageY = window.pageYOffset
     if(pageY > 100 && pageY < 300){
         nav[0].classList.add('nav--closed')
+        nav[0].classList.add('nav--transition')
     }else if(pageY > 300){
+        // nav[0].classList.remove('nav--transition')
         nav[0].classList.remove('nav--closed')
         nav[0].classList.add('nav--scroll')
     }else{
+        nav[0].classList.remove('nav--transition')
         nav[0].classList.remove('nav--closed')
         nav[0].classList.remove('nav--scroll')
     }
